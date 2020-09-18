5bc815cfeaeea84fbbc5e97d4f91ccee6cfff339
Liselle Logan <liselle.logan@gmail.com>
2020-09-18 00:42:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,8 @@
-def my_collect(empty_array)
-  
+def my_collect(array)
+  counter = 0
+  collections = []
+  while counter < array.length
+    collections << yield(counter)
+    counter += 1
+  end
 end
