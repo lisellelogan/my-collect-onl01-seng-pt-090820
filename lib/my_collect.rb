29ead8519cd81801fca4b790fe4a5e631139737e
Liselle Logan <liselle.logan@gmail.com>
2020-09-18 00:56:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   counter = 0
   collections = []
   while counter < array.length
-    collections << yield(array(counter))
+    collections << yield(array[counter])
     counter += 1
   end
   collections
